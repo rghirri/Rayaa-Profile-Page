0625159ab1d8cc9bd074fe294cf8b6b7bd41de38
Rayaa <rghirri@gmail.com>
2020-08-06 14:22:08 +0100

    The Profile Original

---
+++ src/web/themes/custom/blog/js/custom.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+   /*   $(".nav li a").on("click",function(){
+        $(".nav li a").removeClass("active");
+        $(this).addClass("active");
+    });*/
+  
+    });
+    
+  })(jQuery);
